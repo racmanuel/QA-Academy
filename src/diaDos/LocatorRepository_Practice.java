e7804fe3a7079056a0aec73870913f446ac60697
Manuel Ramirez Coronel <ra_cm@outlook.com>
2020-01-15 13:08:00 -0600

    Academia QA Aumenta Software - Dia 1,2, Practica Finalizada

--- src/diaDos/LocatorRepository_Practice.java
+++ src/diaDos/LocatorRepository_Practice.java
@@ -4,7 +4,9 @@ public class LocatorRepository_Practice {
 	final static String pathDriver="C:\\Users\\ra_cm\\eclipse-workspace\\QA-Academy\\src\\Driver\\chromedriver.exe";
 	final static String serchFieldXpath="//input[@type='text']";
 	final static String URL="https://www.google.com";
+	final static String logoGoogle="hplogo";
 	final static String searchResult="div.rc>div.r>a[href*=\"https://selenium.dev/projects/\"]>h3";
+	final static String logoSelenium="#header>.headerLink>.logo-large";
 	final static String pageDownloads="#navbar>a";
 	final static String pageJavaApi="a[href*=\"https://seleniumhq.github.io/selenium/docs/api/java/index.html\"]";
 }
--- src/diaDos/practice.java
+++ src/diaDos/practice.java
@@ -11,38 +11,43 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class practice {
-	
+
 	WebDriver driver;
-	
+
 	@BeforeTest
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", LocatorRepository_Practice.pathDriver);
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 	}
+
 	@Test(priority = 1)
 	public void Practica() {
 		driver.get(LocatorRepository_Practice.URL);
+		WebElement logoGoogle = driver.findElement(By.id(LocatorRepository_Practice.logoGoogle));
+		Assert.assertTrue(logoGoogle.isDisplayed());
 		WebElement search = driver.findElement(By.xpath(LocatorRepository_Practice.serchFieldXpath));
 		search.clear();
 		search.sendKeys("selenium webdriver" + Keys.ENTER);
 		WebElement selenium = driver.findElement(By.cssSelector(LocatorRepository_Practice.searchResult));
 		selenium.click();
+		WebElement logoSelenium = driver.findElement(By.cssSelector(LocatorRepository_Practice.logoSelenium));
+		Assert.assertTrue(logoSelenium.isDisplayed());
 		WebElement selenium_downloads = driver.findElement(By.cssSelector(LocatorRepository_Practice.pageDownloads));
 		selenium_downloads.click();
 		WebElement selenium_java = driver.findElement(By.cssSelector(LocatorRepository_Practice.pageJavaApi));
 		selenium_java.click();
-		System.out.println(driver.getCurrentUrl());
+		String C_URL = driver.getCurrentUrl();
+		Assert.assertEquals(C_URL, "https://selenium.dev/selenium/docs/api/java/index.html");
 	}
-	
-	@Test(priority = 2)
+
 	public void Practica2() {
 		driver.get(LocatorRepository.URL);
 		System.out.println(driver.getTitle());
 		WebElement logoGoogle = driver.findElement(By.id("hplogo"));
 		Assert.assertTrue(logoGoogle.isDisplayed());
 	}
-	
+
 	@AfterTest
 	private void tearDown() {
 		driver.close();
